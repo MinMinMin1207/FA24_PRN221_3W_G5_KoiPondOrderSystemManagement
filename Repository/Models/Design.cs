788e812eb1d586a22b2b139b54afb01c3fd1dcfa
MinMinMin1207 <locnguyen2003vn@gmail.com>
2024-11-27 23:43:28 +0700

    Fix code base

---
+++ Repository/Models/Design.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Repository.Models;
+
+public partial class Design
+{
+    public int DesignId { get; set; }
+
+    public string DesignName { get; set; } = null!;
+
+    public string? DesignStyle { get; set; }
+
+    public decimal? EstimatedCost { get; set; }
+
+    public string? Description { get; set; }
+
+    public string? ImagePath { get; set; }
+
+    public int? PopularityScore { get; set; }
+
+    public string? RecommendedUse { get; set; }
+
+    public int? PromotionId { get; set; }
+
+    public bool ApprovalStatus { get; set; }
+
+    public virtual ICollection<Project> Projects { get; set; } = new List<Project>();
+
+    public virtual Promotion? Promotion { get; set; }
+}
